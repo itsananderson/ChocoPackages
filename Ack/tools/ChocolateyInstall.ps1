532834257b9ad07a3f385bff3b91db28e14fd98a
unknown <will@itsananderson.com>
2014-07-21 15:47:10 -0700

    Add shell script so ack can be invoked from Git Bash

--- Ack/tools/ChocolateyInstall.ps1
+++ Ack/tools/ChocolateyInstall.ps1
@@ -4,7 +4,10 @@ $contentPath = join-path $parentPath 'content'
 $nugetBin = join-path $env:chocolateyinstall 'bin'
 $ackScript = join-path $contentPath 'ack.pl'
 $cmdFile = join-path $contentPath 'ack.cmd'
+$shFile = join-path $contentPath 'ack'
 $fsObject = New-Object -ComObject Scripting.FileSystemObject
 $ackScript = $fsObject.GetFile("$ackScript").ShortPath
 Get-Content $cmdFile | Foreach-Object{$_ -replace 'ACK_SCRIPT', "$ackScript"} | Set-Content 'TempFile.txt'
 move-item 'TempFile.txt' $(join-path $nugetBin 'ack.cmd') -Force
+Get-Content $shFile | Foreach-Object{$_ -replace 'ACK_SCRIPT', "$ackScript"} | Set-Content 'TempFile.txt'
+move-item 'TempFile.txt' $(join-path $nugetBin 'ack') -Force
